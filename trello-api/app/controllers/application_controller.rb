2d5cbfbc0274b4674561e123a01db022c2ca9e97
3elk4 <izaa1109@gmail.com>
2020-10-27 17:35:57 +0100

    sign token to session user id

--- trello-api/app/controllers/application_controller.rb
+++ trello-api/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
 
     def authenticate_request
         @current_user = AuthorizeRequest.call(request.headers).result
-        session[:user_id] = @current_user.id if @current_user
+        session[@current_user.id] = request.headers["Authorization"].split(' ').last if @current_user
         render json: {error: "Not authorized, go away you poor peasant"}, status: 401 unless @current_user
     end
 end
