8f8d979033225994551c8c0dbb507de750af1c49
3elk4 <izaa1109@gmail.com>
2020-10-27 16:36:07 +0100

    add routes to crud for board

--- trello-api/config/routes.rb
+++ trello-api/config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   resources :users
 
   post 'authenticate', to: 'login#authenticate'
+
   get 'index', to: 'board#index'
+  post 'create_board', to: 'board#create'
+  post 'edit_board', to: 'board#edit'
+  post 'delete_board', to: 'board#delete'
+  post 'show_board', to: 'board#show'
 end
