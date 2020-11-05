8d5f50d045b3c6ebd660cb71519f68294fd35c99
Jacek Wychowałek <rysiuziom1@gmail.com>
2020-11-05 19:47:43 +0100

    change JWT lib package

--- lib/src/json_web_token.rb
+++ lib/src/json_web_token.rb
@@ -2,11 +2,11 @@ class JsonWebToken
     class << self
         def encode(payload, expire_time = 24.hours.from_now)
             payload[:expire_time] = expire_time.to_i
-            return JWT.encode(payload, Rails.application.secrets.secret_key_base)
+            return JWT.encode(payload, Rails.application.secret_key_base)
         end
 
         def decode(token)
-            body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+            body = JWT.decode(token, Rails.application.secret_key_base)[0]
             HashWithIndifferentAccess.new body
         end
     end
