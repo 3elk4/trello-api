c54e0e8cf10b8338cf41ecb3f5bf46f7f4bfb1c3
Maciej <gorski.maciej@o2.pl>
2020-11-10 14:40:09 +0100

    added example seeds for lists in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,13 @@
 user = User.create!(username: "geralt", password: "zaraza")
 
 board1 = Board.create!(name: "Sample board", is_public: "true", user_id: user.id)
-board1 = Board.create!(name: "Another sample board", is_public: "false", user_id: user.id)
+board2 = Board.create!(name: "Another sample board", is_public: "false", user_id: user.id)
 
 archived1 = Board.create!(name:"Sample archived board (private)", is_public: "false", user_id: user.id, archiving_date: DateTime.strptime("15-09-2020", '%d-%m-%Y'))
 archived2 = Board.create!(name:"Sample archived board (public)", is_public: "true", user_id: user.id, archiving_date: DateTime.strptime("24-06-1998", '%d-%m-%Y'))
+
+list_board1 = List.create!(name: "Test list 1", board_id: board1.id)
+list_board2 = List.create!(name: "test list 2", board_id: board2.id)
+
+archivedboard_list1 = List.create!(name: "archived1", board_id: archived1.id)
+archivedboard_list2 = List.create!(name: "archived2", board_id: archived2.id)
