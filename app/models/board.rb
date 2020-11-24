d0125b15b1a82a6f2c005082eef7382d734d9e45
BigDinDonMan <34507893+BigDinDonMan@users.noreply.github.com>
2020-11-24 12:44:26 +0100

    added background to board and edited the controller methods accordingly (#12)

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,12 @@
 class Board < ApplicationRecord
+
+    include Rails.application.routes.url_helpers
+
     has_many :lists, dependent: :destroy
     belongs_to :user
+    has_one_attached :background
+
+    def get_background_url
+        url_for(self.background)
+    end
 end
