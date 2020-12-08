60f35d2c74167a1eb22b8f5eaeb6968a995ea92b
Maciej <gorski.maciej@o2.pl>
2020-12-08 12:35:13 +0100

    deleted obsolete print calls

--- app/serializers/card_comment_serializer.rb
+++ app/serializers/card_comment_serializer.rb
@@ -4,8 +4,6 @@ class CardCommentSerializer < ActiveModel::Serializer
   attributes :id, :username, :content
 
   def username
-    print "JSON HERE"
-    print(object.to_json)
     User.find(object.user_id).username
   end
 end
